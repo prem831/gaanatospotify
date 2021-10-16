3ac3279c9c62a4da213da86b0be6d78c918316e9
prem831 <59229947+prem831@users.noreply.github.com>
2021-10-16 14:08:33 +0530

    Update driver_setup.py

--- driver_setup.py
+++ driver_setup.py
@@ -1,13 +1,10 @@
-# import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# os.environ['PATH'] += r"E:/chromedriver"
 def get_driver():
     opt = webdriver.ChromeOptions()
     opt.add_experimental_option("debuggerAddress", "localhost:8989")
     
     driver = webdriver.Chrome(executable_path="E:\\chromedriver\\chromedriver.exe", chrome_options=opt)
-    # driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
     return driver
