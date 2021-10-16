905922e95771311f854b88e983a6e7658a360390
prem831 <59229947+prem831@users.noreply.github.com>
2021-10-16 14:13:01 +0530

    Update gaanamigrate.py

--- gaanamigrate.py
+++ gaanamigrate.py
@@ -1,18 +1,20 @@
 import driver_setup
-
 import time
+
+#first open gaana playlist in one tab and spotify playlist in another tab
+#here i have used window handles wothout any check but if you use this implement for loop
 
 driver = driver_setup.get_driver()
 handles = driver.window_handles
 driver.switch_to.window(handles[1])
 print(driver.title)
-# current_url = "https://gaana.com/playlist/gaanauser89309842-nwyxh-16oct-qk52mj0l3w"
-# driver.get(current_url)
+
 driver.implicitly_wait(10)
 song_names = driver.find_elements_by_xpath('//a/span[@class="t_over"]')
 album_names = driver.find_elements_by_xpath('//div/a[@class="t_over"]')
 l1 = [song.text for song in song_names]
 l2 = [album.text for album in album_names]
+
 not_found = []
 
 driver.switch_to.window(handles[0])
